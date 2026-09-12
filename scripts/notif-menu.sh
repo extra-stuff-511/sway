c0ff862d7aff3127e4df63c940b818149bf2b506
extra-stuff-511 <extra511stuff@proton.me>
2026-09-13 04:44:06 +0530

    Update and rename notif-menu.sh to notif-menu.sh

--- scripts/notif-menu.sh
+++ scripts/notif-menu.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+if pgrep -x rofi >/dev/null; then
+    pkill -x rofi
+    exit 0
+fi
+
 choice=$(
     {
         echo "Clear notifications"
         makoctl history | awk -F '\t' '{print $2}'
-    } | rofi -dmenu -i -p "Notifications"
+    } | rofi -dmenu -i -p "Notifications >"
 )
 
 case "$choice" in
