60b7c5b75d4fbbccec427dc2ba3e30201aab605c
extra-stuff-511 <extra511stuff@proton.me>
2026-09-13 04:43:18 +0530

    Update and rename logout-menu.sh to logout-menu.sh

--- scripts/logout-menu.sh
+++ scripts/logout-menu.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Lock\nLogout\nReboot\nShutdown" | rofi -dmenu -p "Power > ")
+if pgrep -x rofi >/dev/null; then
+    pkill -x rofi
+    exit 0
+fi
+
+choice=$(printf "Lock\nLogout\nReboot\nShutdown" | rofi -dmenu -p "Power >")
 
 case "$choice" in
     Lock)     loginctl lock-session ;;
