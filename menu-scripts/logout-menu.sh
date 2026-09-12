3723a39b5e9d2d8b39e695392980d87e667b45d7
extra-stuff-511 <extra511stuff@proton.me>
2026-09-12 19:50:24 +0530

    Update logout-menu.sh

--- menu-scripts/logout-menu.sh
+++ menu-scripts/logout-menu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Lock\nLogout\nReboot\nShutdown" | rofi -dmenu -p "Power")
+choice=$(printf "Lock\nLogout\nReboot\nShutdown" | rofi -dmenu -p "Power > ")
 
 case "$choice" in
     Lock)     loginctl lock-session ;;
