4002954ec109c4e18afac734e94c6b121c90cd8e
extra-stuff-511 <extra511stuff@proton.me>
2026-09-13 04:45:15 +0530

    Update and rename ppf-menu.sh to ppf-menu.sh

--- scripts/ppf-menu.sh
+++ scripts/ppf-menu.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+if pgrep -x rofi >/dev/null; then
+    pkill -x rofi
+    exit 0
+fi
+
 current=$(powerprofilesctl get)
 
 choice=$(
     {
         printf 'Current: %s\n' "$current"
         powerprofilesctl list | awk -F: '/^[* ]+[a-z-]+:/ {gsub(/^[* ]+/, "", $1); print $1}'
-    } | rofi -dmenu -p 'Power Profile > '
+    } | rofi -dmenu -p 'Power Profile >'
 )
 
 [[ $choice == Current:* || -z $choice ]] || powerprofilesctl set "$choice"
